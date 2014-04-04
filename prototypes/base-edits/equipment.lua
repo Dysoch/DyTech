18add661969b23b242bf8bc7e27126401e4e2fde
Benjamin Clements <HJP_Ben1993@yahoo.com>
2014-04-03 22:30:12 -0500

    equipment base-edit fix

--- prototypes/base-edits/equipment.lua
+++ prototypes/base-edits/equipment.lua
@@ -1,2 +1,2 @@
-data.raw["active-defense-equipment"]["basic-laser-defense-equipment"].filename = "__DyTech-Graphics__/graphics/equipment/basic-laser-defense-equipment-1.png"
-data.raw["active-defense-equipment"]["basic-laser-defense-equipment"].projectile = "laser-ruby-1"
+data.raw["active-defense-equipment"]["basic-laser-defense-equipment"].sprite.filename = "__DyTech-Graphics__/graphics/equipment/basic-laser-defense-equipment-1.png"
+data.raw["active-defense-equipment"]["basic-laser-defense-equipment"].attack_parameters.ammo_type.action[1].action_delivery[1].projectile = "laser-ruby-1"
