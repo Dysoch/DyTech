74026431267e1761d46c63aff4a12ff93901e211
TerraSleet <thestefan@hotmail.co.uk>
2014-06-30 13:57:48 +0100

    Syntax fixes
    Removed the if statement, I don't know its purpose and it also had a syntax error.
    Also fixed a missing {

--- DyTech-Metallurgy/control.lua
+++ DyTech-Metallurgy/control.lua
@@ -13,9 +13,10 @@ game.onload(function()
 end)
 
 game.onevent(defines.events.ontick, function(event)
-	if glob.dytech.metallurgy=false then glob.dytech.metallurgy=true end
+	if glob.dytech.metallurgy==false then glob.dytech.metallurgy=true end
 end)
 
 remote.addinterface("DyTech-Metallurgy",
+	{
 
 })
