0ceddd64315b4bf5119a70e250baed12fb4b1472
TerraSleet <thestefan@hotmail.co.uk>
2014-06-30 14:03:31 +0100

    Syntax fixes

--- DyTech-Compatibility/control.lua
+++ DyTech-Compatibility/control.lua
@@ -13,9 +13,10 @@ game.onload(function()
 end)
 
 game.onevent(defines.events.ontick, function(event)
-	if glob.dytech.compatibility=false then glob.dytech.compatibility=true end
+	
 end)
 
 remote.addinterface("DyTech-Compatibility",
+	{
 
 })
