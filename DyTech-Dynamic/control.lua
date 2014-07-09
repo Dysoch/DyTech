92192494b7d460622dcd1ad9c05287251cbdfe4f
Dysoch <frits@devreugd.nl>
2014-07-09 21:01:39 +0200

    preliminary script finished
    testing tomorrow! Now i quickly start on a few Energy based entities
    before the World Cup match!

--- DyTech-Dynamic/control.lua
+++ DyTech-Dynamic/control.lua
@@ -2,6 +2,7 @@ require "defines"
 
 game.oninit(function()
 	remote.call("DyTech-Core", "addModule", "dynamic")
+glob.modules={core=false, energy=false, inserters=false, logistic=false, metallurgy=false, meteors=false, mining=false, modules=false, storage=false, tools=false, transportation=false, warfare=false, compatibility=false}
 end)
 
 game.onsave(function()
@@ -16,20 +17,18 @@ game.onevent(defines.events.ontick, function(event)
 	--[[Checking to see which modules are installed. This happens every hour!]]--
 	if event.tick%216000==0 then
 		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true else glob.modules.core = false end 
-		if remote.call("DyTech-Core", "detectModule", "compatibility")==true then glob.modules.compatibility = true end 
-		if remote.call("DyTech-Core", "detectModule", "energy")==true then glob.modules. = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
-		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "compatibility")==true then glob.modules.compatibility = true else glob.modules.compatibility = false end 
+		if remote.call("DyTech-Core", "detectModule", "energy")==true then glob.modules.energy = true else glob.modules.energy = false end 
+		if remote.call("DyTech-Core", "detectModule", "inserters")==true then glob.modules.inserters = true else glob.modules.inserters = false end 
+		if remote.call("DyTech-Core", "detectModule", "logistic")==true then glob.modules.logistic = true else glob.modules.logistic = false end 
+		if remote.call("DyTech-Core", "detectModule", "metallurgy")==true then glob.modules.metallurgy = true else glob.modules.metallurgy = false end 
+		if remote.call("DyTech-Core", "detectModule", "meteors")==true then glob.modules.meteors = true else glob.modules.meteors = false end 
+		if remote.call("DyTech-Core", "detectModule", "mining")==true then glob.modules.mining = true else glob.modules.mining = false end 
+		if remote.call("DyTech-Core", "detectModule", "modules")==true then glob.modules.modules = true else glob.modules.modules = false end 
+		if remote.call("DyTech-Core", "detectModule", "storage")==true then glob.modules.storage = true else glob.modules.storage = false end 
+		if remote.call("DyTech-Core", "detectModule", "tools")==true then glob.modules.tools = true else glob.modules.tools = false end 
+		if remote.call("DyTech-Core", "detectModule", "transportation")==true then glob.modules.transportation = true else glob.modules.transportation = false end 
+		if remote.call("DyTech-Core", "detectModule", "warfare")==true then glob.modules.warfare = true else glob.modules.warfare = false end 
 	end
 end)
 
