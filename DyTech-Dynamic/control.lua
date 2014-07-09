194bd1e8a445e4839ec0ceac3646243577f89a67
Dysoch <frits@devreugd.nl>
2014-07-09 15:25:04 +0200

    preliminary script.
    have to test it extensively to make sure it works the way i want to. and
    besides, its not even finished yet xD

--- DyTech-Dynamic/control.lua
+++ DyTech-Dynamic/control.lua
@@ -13,7 +13,24 @@ game.onload(function()
 end)
 
 game.onevent(defines.events.ontick, function(event)
-	
+	--[[Checking to see which modules are installed. This happens every hour!]]--
+	if event.tick%216000==0 then
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true else glob.modules.core = false end 
+		if remote.call("DyTech-Core", "detectModule", "compatibility")==true then glob.modules.compatibility = true end 
+		if remote.call("DyTech-Core", "detectModule", "energy")==true then glob.modules. = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+		if remote.call("DyTech-Core", "detectModule", "core")==true then glob.modules.core = true end 
+	end
 end)
 
 remote.addinterface("DyTech-Dynamic",
