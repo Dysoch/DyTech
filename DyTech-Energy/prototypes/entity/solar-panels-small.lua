dc35d3cf7a1dd3b43284438578f10fd49c1d7b54
Dysoch <frits@devreugd.nl>
2014-07-10 11:20:10 +0200

    starting on energy to add more steam, solar, accu

---
+++ DyTech-Energy/prototypes/entity/solar-panels-small.lua
@@ -0,0 +1,54 @@
+data:extend(
+{
+  {
+    type = "solar-panel",
+    name = "solar-panel-secondary",
+    icon = "__base__/graphics/icons/solar-panel.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "solar-panel-secondary"},
+    max_health = 100,
+    corpse = "big-remnants",
+    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+	fast_replaceable_group =  "solar-panel",
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-output"
+    },
+    picture =
+    {
+      filename = "__base__/graphics/entity/solar-panel/solar-panel.png",
+      priority = "high",
+      width = 104,
+      height = 96
+    },
+    production = "60kW"
+  },
+  {
+    type = "solar-panel",
+    name = "solar-panel-terciary",
+    icon = "__base__/graphics/icons/solar-panel.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "solar-panel-terciary"},
+    max_health = 100,
+    corpse = "big-remnants",
+    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+	fast_replaceable_group =  "solar-panel",
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "terciary"
+    },
+    picture =
+    {
+      filename = "__base__/graphics/entity/solar-panel/solar-panel.png",
+      priority = "high",
+      width = 104,
+      height = 96
+    },
+    production = "60kW"
+  },
+}
+)
