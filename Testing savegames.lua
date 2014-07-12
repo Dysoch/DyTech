7ec9c40a5e2def2c78190ec99428c59402c94dab
Dysoch <frits@devreugd.nl>
2014-07-12 16:10:20 +0200

    Savegame counter calc

--- Testing savegames.lua
+++ Testing savegames.lua
@@ -1,12 +1,12 @@
 Dysoch ={
 counter={
-resource=5013,science=601,tech=10856,gun=1,plates=34746,sectorscanned=0,dytech=61630,gear=4303,machine=643,mining=245,wall=0,ammo=95,robot=208,capsule=0,gems=0,belt=2127,chest=458,alien=300,turret=0,armor=0,energy=779,modules=0,inserter=1255},
-combat={
-dog=0,berserker=0,elder=0,small=1847,queen=0,big=413,bird=0,medium=908,king=0,dytech=3168}
+gear=4747,resource=5668,gun=1,wall=0,inserter=1257,belt=2127,energy=1701,science=601,plates=44725.5,modules=0,chest=458,tech=13343.5,armor=0,mining=245,gems=0,robot=208,ammo=95,alien=300,dytech=76406,turret=0,capsule=0,sectorscanned=0,machine=929}
 counter2={
-died=3471,pickup=6842,mine=9840,sectorscanned=11983,chunks=2973,build=4472,dytech=39581}
+died=4837,mine=10230,dytech=48681,pickup=11595,build=4875,sectorscanned=14106,chunks=3039}
 dytech={
-core=true,storage=true,compatibility=false,energy=true,inserters=true,dynamic=false,logistic=true,meteors=false,mining=true,modules=true,tools=true,metallurgy=true,warfare=true,transportation=true}}
+dynamic=false,tools=true,energy=true,transportation=true,logistic=true,metallurgy=true,warfare=true,storage=true,meteors=false,mining=true,modules=true,core=true,compatibility=false,inserters=true}
+combat={
+dog=0,bird=0,small=2346,king=0,queen=0,dytech=4243,elder=52,berserker=0,big=654,medium=1192}}
 
 Grand = {
 combat={
