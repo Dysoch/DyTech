bdce4e68c7668ea93b1664196e92b48fbf480dfd
Dysoch <frits@devreugd.nl>
2014-07-13 16:46:29 +0200

    Database 4
    Database DONE! (and not a moment to soon xD, was going nuts of only
    looking at code xD)

--- DyTech-Dynamic/scripts/functions.lua
+++ DyTech-Dynamic/scripts/functions.lua
@@ -51,7 +51,7 @@ dytech=0, boiler1=0, boiler2=0, boiler3=0, boiler4=0, boiler5=0, smallpole=0, me
 glob.crafted.inserters={
 dytech=0, burner=0, basic=0, fast=0, long=0, smart=0, half=0, veryfast=0}
 glob.crafted.intermediates={
-dytech=0, stick=0, wood=0, circuit1=0, circuit2=0, circuit3=0, circuit4=0, battery=0, cable=0, wirered=0, wiregreen=0, bundledwire=0, gear1=0, gear2=0, gear3=0, plastic=0, engine1=0, engine2=0, robotframe=0, explosives=0, gearmetallurgy=0}
+dytech=0, stick=0, wood=0, circuit1=0, circuit2=0, circuit3=0, circuit4=0, battery=0, cable=0, wirered=0, wiregreen=0, bundledwire=0, gear1=0, gear2=0, gear3=0, plastic=0, engine1=0, engine2=0, robotframe=0, explosives=0, gearmetallurgy=0, frames=0, itemexit=0, rotors=0, blades=0}
 glob.crafted.guns={
 dytech=0, pistol=0, submachine=0, shotgun=0, combatshotgun=0, sniper=0, railgun=0, flamethrower=0, rocket=0, mine=0}
 glob.crafted.ammo={
@@ -63,7 +63,7 @@ dytech=0, wooden=0, stone=0, copper=0, iron=0, steel=0, advsteel=0, sapphire=0, 
 glob.crafted.furnaces={
 dytech=0, stone=0, steel=0, electric=0, forge=0, blastfurnace=0}
 glob.crafted.transport={
-dytech=0, belt1=0, belt2=0, belt3=0, belt4=0, belt5=0, underbelt1=0, underbelt2=0, underbelt3=0, underbelt4=0, underbelt5=0, splitter1=0, splitter2=0, splitter3=0, splitter4=0, splitter5=0, pipe1=0, pipe2=0, pipe3=0, pipe4=0, pipe5=0, underpipe1=0, underpipe2=0, underpipe3=0, underpipe4=0, underpipe5=0, pump1=0, pump2=0, pump3=0, pump4=0, pump5=0, car1=0, car2=0, rail1=0, rail2=0, diesel1=0, diesel2=0, diesel3=0, wagon1=0, wagon2=0, wagon3=0, stop=0, signal=0, tank1=0. pipebig=0, pumpbig=0}
+dytech=0, belt1=0, belt2=0, belt3=0, belt4=0, belt5=0, underbelt1=0, underbelt2=0, underbelt3=0, underbelt4=0, underbelt5=0, splitter1=0, splitter2=0, splitter3=0, splitter4=0, splitter5=0, pipe1=0, pipe2=0, pipe3=0, pipe4=0, pipe5=0, underpipe1=0, underpipe2=0, underpipe3=0, underpipe4=0, underpipe5=0, pump1=0, pump2=0, pump3=0, pump4=0, pump5=0, car1=0, car2=0, rail1=0, rail2=0, diesel1=0, diesel2=0, diesel3=0, wagon1=0, wagon2=0, wagon3=0, wagon4=0, stop=0, signal=0, tank1=0. pipebig=0, pumpbig=0}
 glob.crafted.miners={
 dytech=0, burner=0, basic1=0, basic2=0, basic3=0, basic4=0, basic5=0, basic6=0, gems=0}
 glob.crafted.armor={
@@ -71,7 +71,7 @@ dytech=0, basic=0, heavy=0, modular1=0, modular2=0, modular3=0, modular4=0, nigh
 glob.crafted.capsules={
 dytech=0, poison=0, slowdown=0, grenade=0, defender=0, distractor=0, destroyer=0, discharge=0}
 glob.crafted.crafters={
-dytech=0, assem1=0, assem2=0, assem3=0, pumpjack=0, refinery=0, chemical=0, molds=0, airintake=0, centrifuge=0, geothermal=0, lavahandler=0}
+dytech=0, assem1=0, assem2=0, assem3=0, pumpjack=0, refinery=0, chemical=0, molds=0, airintake=0, centrifuge=0, geothermal=0, lavahandler=0, gem1=0, gem2=0, gem3=0, gem4=0}
 glob.crafted.logistics={
 dytech=0, repair1=0, repair2=0, repair3=0, robot1=0, robot2=0, robot3=0, chest1=0, chest2=0, chest3=0, port1=0, port2=0, port3=0, blueprint=0, deconstruct=0}
 glob.crafted.turrets={
@@ -83,7 +83,7 @@ dytech=0, lab=0, science1=0, science2=0, science3=0, science4=0, alienscience=0}
 glob.crafted.nature={
 dytech=0, rubberseed=0}
 glob.crafted.others={
-dytech=0, radar=0, lamp=0, port=0, wall=0, rocketdefense=0, beacon=0, plane=0, barrel=0, collectors=0}
+dytech=0, radar=0, lamp=0, port=0, wall=0, rocketdefense=0, beacon=0, plane=0, barrel=0, collectors=0, alien=0}
 end
 
 function ModuleCheck()
