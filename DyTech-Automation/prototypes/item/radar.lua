a1014e16f34883595d818f63685cd20782a0f97c
Dysoch <frits@devreugd.nl>
2014-07-25 11:19:56 +0200

    [Automation] Items

---
+++ DyTech-Automation/prototypes/item/radar.lua
@@ -0,0 +1,44 @@
+data:extend(
+{ 
+  {
+    type = "item",
+    name = "radar-mk2",
+    icon = "__DyTech-Automation__/graphics/icons/radar-2.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "defensive-structure",
+    order = "d[radar]-a[radar]-2",
+    place_result = "radar-mk2",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "radar-mk3",
+    icon = "__DyTech-Automation__/graphics/icons/radar-3.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "defensive-structure",
+    order = "d[radar]-a[radar]-3",
+    place_result = "radar-mk3",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "radar-mk4",
+    icon = "__DyTech-Automation__/graphics/icons/radar-4.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "defensive-structure",
+    order = "d[radar]-a[radar]-4",
+    place_result = "radar-mk4",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "radar-mk5",
+    icon = "__DyTech-Automation__/graphics/icons/radar-5.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "defensive-structure",
+    order = "d[radar]-a[radar]-5",
+    place_result = "radar-mk5",
+    stack_size = 50
+  },
+}
+)
