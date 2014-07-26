0e3374bf7af7b580b93791aae813415a4653e345
Dysoch <frits@devreugd.nl>
2014-07-26 09:40:25 +0200

    [Warfare] Loot as item

---
+++ DyTech-Warfare/prototypes/item/corpses.lua
@@ -0,0 +1,40 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "biter-berserk-corpse",
+    icon = "__DyTech-Warfare__/graphics/icons/biter-berserk-corpse.png",
+    flags = {"goes-to-inventory"},
+    subgroup = "dytechloot",
+    order = "1-berserk",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "biter-elder-corpse",
+    icon = "__DyTech-Warfare__/graphics/icons/biter-elder-corpse.png",
+    flags = {"goes-to-inventory"},
+    subgroup = "dytechloot",
+    order = "2-elder",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "biter-king-corpse",
+    icon = "__DyTech-Warfare__/graphics/icons/biter-king-corpse.png",
+    flags = {"goes-to-inventory"},
+    subgroup = "dytechloot",
+    order = "3-king",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "biter-queen-corpse",
+    icon = "__DyTech-Warfare__/graphics/icons/biter-queen-corpse.png",
+    flags = {"goes-to-inventory"},
+    subgroup = "dytechloot",
+    order = "4-queen",
+    stack_size = 50
+  },
+}
+)
