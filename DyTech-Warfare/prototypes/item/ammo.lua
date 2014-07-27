51b996c53d31734f1b0c83694196ef93280100a4
Dysoch <frits@devreugd.nl>
2014-07-27 20:15:03 +0200

    [Warfare] Balancing

--- DyTech-Warfare/prototypes/item/ammo.lua
+++ DyTech-Warfare/prototypes/item/ammo.lua
@@ -45,159 +45,7 @@ data:extend(
     flags = {"goes-to-main-inventory"},
     ammo_type =
     {
-      category = "gem-ammo",
-      action =
-      {
-        type = "direct",
-        action_delivery =
-        {
-          type = "instant",
-          source_effects =
-          {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-          },
-          target_effects =
-          {
-            {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-            },
-            {
-              type = "damage",
-              damage = { amount = 50 , type = "physical"}
-            }
-          }
-        }
-      }
-    },
-    magazine_size = 50,
-    subgroup = "ammo",
-    order = "gem-diamond",
-    stack_size = 100
-  },
-  {
-    type = "ammo",
-    name = "emerald-bullet-magazine",
-    icon = "__DyTech-Warfare__/graphics/icons/emerald-bullet-magazine.png",
-    flags = {"goes-to-main-inventory"},
-    ammo_type =
-    {
-      category = "gem-ammo",
-      action =
-      {
-        type = "direct",
-        action_delivery =
-        {
-          type = "instant",
-          source_effects =
-          {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-          },
-          target_effects =
-          {
-            {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-            },
-            {
-              type = "damage",
-              damage = { amount = 35 , type = "physical"}
-            }
-          }
-        }
-      }
-    },
-    magazine_size = 50,
-    subgroup = "ammo",
-    order = "gem-emerald",
-    stack_size = 100
-  },
-  {
-    type = "ammo",
-    name = "topaz-bullet-magazine",
-    icon = "__DyTech-Warfare__/graphics/icons/topaz-bullet-magazine.png",
-    flags = {"goes-to-main-inventory"},
-    ammo_type =
-    {
-      category = "gem-ammo",
-      action =
-      {
-        type = "direct",
-        action_delivery =
-        {
-          type = "instant",
-          source_effects =
-          {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-          },
-          target_effects =
-          {
-            {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-            },
-            {
-              type = "damage",
-              damage = { amount = 30 , type = "physical"}
-            }
-          }
-        }
-      }
-    },
-    magazine_size = 50,
-    subgroup = "ammo",
-    order = "gem-topaz",
-    stack_size = 100
-  },
-  {
-    type = "ammo",
-    name = "ruby-bullet-magazine",
-    icon = "__DyTech-Warfare__/graphics/icons/ruby-bullet-magazine.png",
-    flags = {"goes-to-main-inventory"},
-    ammo_type =
-    {
-      category = "gem-ammo",
-      action =
-      {
-        type = "direct",
-        action_delivery =
-        {
-          type = "instant",
-          source_effects =
-          {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-          },
-          target_effects =
-          {
-            {
-              type = "create-entity",
-              entity_name = "explosion-gunshot"
-            },
-            {
-              type = "damage",
-              damage = { amount = 20 , type = "physical"}
-            }
-          }
-        }
-      }
-    },
-    magazine_size = 50,
-    subgroup = "ammo",
-    order = "gem-ruby",
-    stack_size = 100
-  },
-  {
-    type = "ammo",
-    name = "sapphire-bullet-magazine",
-    icon = "__DyTech-Warfare__/graphics/icons/sapphire-bullet-magazine.png",
-    flags = {"goes-to-main-inventory"},
-    ammo_type =
-    {
-      category = "gem-ammo",
+      category = "bullet",
       action =
       {
         type = "direct",
@@ -218,6 +66,158 @@ data:extend(
             {
               type = "damage",
               damage = { amount = 25 , type = "physical"}
+            }
+          }
+        }
+      }
+    },
+    magazine_size = 50,
+    subgroup = "ammo",
+    order = "gem-diamond",
+    stack_size = 100
+  },
+  {
+    type = "ammo",
+    name = "emerald-bullet-magazine",
+    icon = "__DyTech-Warfare__/graphics/icons/emerald-bullet-magazine.png",
+    flags = {"goes-to-main-inventory"},
+    ammo_type =
+    {
+      category = "bullet",
+      action =
+      {
+        type = "direct",
+        action_delivery =
+        {
+          type = "instant",
+          source_effects =
+          {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+          },
+          target_effects =
+          {
+            {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+            },
+            {
+              type = "damage",
+              damage = { amount = 17.5 , type = "physical"}
+            }
+          }
+        }
+      }
+    },
+    magazine_size = 50,
+    subgroup = "ammo",
+    order = "gem-emerald",
+    stack_size = 100
+  },
+  {
+    type = "ammo",
+    name = "topaz-bullet-magazine",
+    icon = "__DyTech-Warfare__/graphics/icons/topaz-bullet-magazine.png",
+    flags = {"goes-to-main-inventory"},
+    ammo_type =
+    {
+      category = "bullet",
+      action =
+      {
+        type = "direct",
+        action_delivery =
+        {
+          type = "instant",
+          source_effects =
+          {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+          },
+          target_effects =
+          {
+            {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+            },
+            {
+              type = "damage",
+              damage = { amount = 15 , type = "physical"}
+            }
+          }
+        }
+      }
+    },
+    magazine_size = 50,
+    subgroup = "ammo",
+    order = "gem-topaz",
+    stack_size = 100
+  },
+  {
+    type = "ammo",
+    name = "ruby-bullet-magazine",
+    icon = "__DyTech-Warfare__/graphics/icons/ruby-bullet-magazine.png",
+    flags = {"goes-to-main-inventory"},
+    ammo_type =
+    {
+      category = "bullet",
+      action =
+      {
+        type = "direct",
+        action_delivery =
+        {
+          type = "instant",
+          source_effects =
+          {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+          },
+          target_effects =
+          {
+            {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+            },
+            {
+              type = "damage",
+              damage = { amount = 10 , type = "physical"}
+            }
+          }
+        }
+      }
+    },
+    magazine_size = 50,
+    subgroup = "ammo",
+    order = "gem-ruby",
+    stack_size = 100
+  },
+  {
+    type = "ammo",
+    name = "sapphire-bullet-magazine",
+    icon = "__DyTech-Warfare__/graphics/icons/sapphire-bullet-magazine.png",
+    flags = {"goes-to-main-inventory"},
+    ammo_type =
+    {
+      category = "bullet",
+      action =
+      {
+        type = "direct",
+        action_delivery =
+        {
+          type = "instant",
+          source_effects =
+          {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+          },
+          target_effects =
+          {
+            {
+              type = "create-entity",
+              entity_name = "explosion-gunshot"
+            },
+            {
+              type = "damage",
+              damage = { amount = 12.5 , type = "physical"}
             }
           }
         }
