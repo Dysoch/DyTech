9492c92301d500f58b6880524969ef3c266c3e0c
Dysoch <frits@devreugd.nl>
2014-09-15 20:13:02 +0200

    [Transportation] Order fix of items

--- DyTech-Transportation/prototypes/item/transport-belt.lua
+++ DyTech-Transportation/prototypes/item/transport-belt.lua
@@ -6,7 +6,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/super-transport-belt.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-4",
+    order = "a[transport-belt]-d[super-transport-belt]",
     place_result = "super-transport-belt",
     stack_size = 50
   },
@@ -16,7 +16,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/extreme-transport-belt.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-5",
+    order = "a[transport-belt]-e[extreme-transport-belt]",
     place_result = "extreme-transport-belt",
     stack_size = 50
   },
@@ -26,7 +26,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/super-transport-belt-to-ground.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-u-4",
+    order = "b[transport-belt-to-ground]-d[super-transport-belt-to-ground]",
     place_result = "super-transport-belt-to-ground",
     stack_size = 50
   },
@@ -36,7 +36,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/extreme-transport-belt-to-ground.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-u-5",
+    order = "b[transport-belt-to-ground]-e[extreme-transport-belt-to-ground]",
     place_result = "extreme-transport-belt-to-ground",
     stack_size = 50
   },
@@ -46,7 +46,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/super-splitter.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-s-4",
+    order = "c[splitter]-d[super-splitter]",
     place_result = "super-splitter",
     stack_size = 50
   },
@@ -56,7 +56,7 @@ data:extend(
     icon = "__DyTech-Transportation__/graphics/icons/extreme-splitter.png",
     flags = {"goes-to-quickbar"},
     subgroup = "belt",
-    order = "t-b-s-5",
+    order = "c[splitter]-e[extreme-splitter]",
     place_result = "extreme-splitter",
     stack_size = 50
   },
