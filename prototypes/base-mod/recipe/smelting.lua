e10d47946cb78f031d7a98f13fe7d2e59810be51
Dysoch <frits@devreugd.nl>
2014-09-15 20:23:48 +0200

    [DyTech] Cleanup and new map for savegame info

--- prototypes/base-mod/recipe/smelting.lua
+++ prototypes/base-mod/recipe/smelting.lua
@@ -2,15 +2,6 @@ data:extend(
 {
   {
     type = "recipe",
-    name = "rubber",
-	enabled = false,
-    category = "smelting",
-    energy_required = 3.5,--old: 100
-    ingredients = {{ "rubber-tree-resin", 1}},
-    result = "rubber"
-  },
-  {
-    type = "recipe",
     name = "glass",
     category = "smelting",
     energy_required = 3.5,
