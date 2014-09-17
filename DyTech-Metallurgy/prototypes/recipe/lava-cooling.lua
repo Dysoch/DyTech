a7d48f92857e5ff1a3eb42bb537269f4f1f1b64b
Dysoch <frits@devreugd.nl>
2014-09-17 12:44:13 +0200

    [Metallurgy] Lava Cooling

---
+++ DyTech-Metallurgy/prototypes/recipe/lava-cooling.lua
@@ -0,0 +1,259 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "lava-800-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 2,
+    ingredients =
+    {
+	  {type="fluid", name="lava-800", amount=0.2},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-600", amount=0.15}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-1000",
+	enabled = false,
+	category = "lava",
+    energy_required = 2.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-1000", amount=0.25},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-800", amount=0.2}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-1200-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 3,
+    ingredients =
+    {
+	  {type="fluid", name="lava-1200", amount=0.3},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-1000", amount=0.25}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-1400-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 3.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-1400", amount=0.35},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-1200", amount=0.3}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-1600-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 4,
+    ingredients =
+    {
+	  {type="fluid", name="lava-1600", amount=0.4},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-1400", amount=0.35}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-1800-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 4.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-1800", amount=0.45},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-1600", amount=0.4}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-2000-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-2000", amount=0.5},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-1800", amount=0.45}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-2200-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 5.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-2200", amount=0.55},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-2000", amount=0.5}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-2400-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 6,
+    ingredients =
+    {
+	  {type="fluid", name="lava-2400", amount=0.6},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-2200", amount=0.55}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-2600-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 6.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-2600", amount=0.65},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-2400", amount=0.6}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-2800-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 7,
+    ingredients =
+    {
+	  {type="fluid", name="lava-2800", amount=0.7},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-2600", amount=0.65}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-3000-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 7.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-3000", amount=0.75},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-2800", amount=0.7}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-3200-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 8,
+    ingredients =
+    {
+	  {type="fluid", name="lava-3200", amount=0.8},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-3000", amount=0.75}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-3400-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 8.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-3400", amount=0.85},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-3200", amount=0.8}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-3600-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 9,
+    ingredients =
+    {
+	  {type="fluid", name="lava-3600", amount=0.9},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-3400", amount=0.85}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-3800-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 9.5,
+    ingredients =
+    {
+	  {type="fluid", name="lava-3800", amount=0.95},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-3600", amount=0.9}
+	}
+  },
+  {
+    type = "recipe",
+    name = "lava-4000-cooling",
+	enabled = false,
+	category = "lava",
+    energy_required = 10,
+    ingredients =
+    {
+	  {type="fluid", name="lava-4000", amount=1},
+    },
+    results = 
+	{
+	  {type="fluid", name="lava-3800", amount=0.95}
+	}
+  },
+}
+)
