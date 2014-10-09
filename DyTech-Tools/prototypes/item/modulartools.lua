96925e0c7ce1809746429d93b13e0063449c3ba4
Dysoch <frits@devreugd.nl>
2014-10-09 13:09:34 +0200

    [Tools] Proper Modular Tools

--- DyTech-Tools/prototypes/item/modulartools.lua
+++ DyTech-Tools/prototypes/item/modulartools.lua
@@ -3,7 +3,7 @@ require "scripts/tools-database"
 -- Database: Work in progress!!!!
 -- Strength, Hold, Flexibility and Density have a number for 0 to 10. 0 means a modifier of 0.2 (reduces the stat) and 10 means modifier of 2. 5 is neither an increase or reduction!
 -- Flexibility only applies when used as a rod!!!!!!!!!!!!
--- Strength applies when used as a rod or head!!!!!!!!!!!!!
+-- Strength applies when used as a rod!!!!!!!!!!!!!
 -- Hold applied when used as a handle only!!!!!!!!!!!!!!!!
 -- Strength to durability value:
 -- strength of 0: 500 durability before modifiers
@@ -27,10 +27,10 @@ data.raw.item.wood.modularInfo = {
   durability = 1000, --the main durability before it is modified. only applies when used as head!
   mininglevel = 1, --main damage value (aka mining level) before modifiers!
   miningspeed = 1, --main speed value before modifiers!
-  strength = 1,    --mining level*0.4 (reduction)(when used as head)
+  strength = 0.4,    --mining level*0.4 (reduction)(when used as head)
               --durability*0.4 (reduction)(when used as rod)
-  hold = 1, --mining speed*0.4 (reduction)
-  flexibility = 2
+  hold = 0.4, --mining speed*0.4 (reduction)
+  flexibility = 0.4
 }
 data.raw.item.stone.modularInfo = {
   handle = true, 
@@ -39,9 +39,9 @@ data.raw.item.stone.modularInfo = {
   durability = 1500, 
   mininglevel = 2,
   miningspeed = 3, 
-  strength = 2,    
-  hold = 1,
-  flexibility = 0
+  strength = 0.8,    
+  hold = 0.6,
+  flexibility = 0.4
 }
 data.raw.item["iron-plate"].modularInfo = {
   handle = true, 
@@ -50,9 +50,9 @@ data.raw.item["iron-plate"].modularInfo = {
   durability = 2500,
   mininglevel = 3,
   miningspeed = 3,
-  strength = 4,
-  hold = 1,
-  flexibility = 2
+  strength = 1,
+  hold = 0.4,
+  flexibility = 0.8
 }
 data.raw.item["copper-plate"].modularInfo = {
   handle = true, 
@@ -61,9 +61,9 @@ data.raw.item["copper-plate"].modularInfo = {
   durability = 2000, 
   mininglevel = 2, 
   miningspeed = 3, 
-  strength = 3,
-  hold = 1,
-  flexibility = 2
+  strength = 0.8,
+  hold = 0.4,
+  flexibility = 0.6
 }
 data.raw.item["steel-plate"].modularInfo = {
   handle = true, 
@@ -72,9 +72,9 @@ data.raw.item["steel-plate"].modularInfo = {
   durability = 4500,
   mininglevel = 4,
   miningspeed = 3,
-  strength = 6,   
-  hold = 1,
-  flexibility = 2
+  strength = 1.4,   
+  hold = 0.4,
+  flexibility = 0.6
 }
 data.raw.item.rubber.modularInfo = {
   handle = true, 
@@ -83,9 +83,9 @@ data.raw.item.rubber.modularInfo = {
   durability = 500,
   mininglevel = 1,
   miningspeed = 1, 
-  strength = 0,   
-  hold = 10,
-  flexibility = 8
+  strength = 0.2,   
+  hold = 2,
+  flexibility = 1.8
 }
 ToolsDatabase.makeModularPart(data.raw.item.wood)
 ToolsDatabase.makeModularPart(data.raw.item.stone)
