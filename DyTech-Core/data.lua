604ba7640f792ea3aaf4f8ee167383e3b092842c
Dysoch <frits@devreugd.nl>
2014-10-13 20:50:20 +0200

    [Core] New Technologies

--- DyTech-Core/data.lua
+++ DyTech-Core/data.lua
@@ -79,6 +79,9 @@ require("prototypes.recipe.collector")
 require("prototypes.recipe.intermediate")
 require("prototypes.recipe.wall")
 
+-- Tech
+require("prototypes.technology.player")
+
 for k, v in pairs(data.raw.module) do
 	if v.name:find("productivity%-module") and v.limitation then
 		table.insert(v.limitation, "advanced-processing-unit")
--- DyTech-Core/prototypes/base-edit/entity.lua
+++ DyTech-Core/prototypes/base-edit/entity.lua
@@ -1,2 +1,3 @@
 data.raw["resource"]["stone"].richness_multiplier = 13000
 data.raw["resource"]["stone"].richness_base = 300
+data.raw["player"]["player"].inventory_size = 100
