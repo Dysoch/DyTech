c3f9947d1c11d4515a0787cfa31921aa333008a6
Dysoch <frits@devreugd.nl>
2014-10-17 10:16:33 +0200

    [Metallurgy] Tiered Centrifuge
    they can only be unlocked by Dynamic System!

--- DyTech-Metallurgy/prototypes/recipe/machines.lua
+++ DyTech-Metallurgy/prototypes/recipe/machines.lua
@@ -3,17 +3,68 @@ data:extend(
   {
    	type = "recipe",
    	name = "centrifuge",
-	energy_required = 20,
+	energy_required = 10,
     enabled = false,
-    subgroup = "metallurgy-machines",
 	ingredients = 
 	{ 
 	  {"iron-plate", 25},
-	  {"steel-plate", 25},
-	  {"steel-gear-wheel", 25},
+	  {"steel-plate", 10},
+	  {"iron-gear-wheel", 25},
 	  {"copper-cable", 10}
 	},
    	result = "centrifuge",
+  },
+  {
+   	type = "recipe",
+   	name = "centrifuge-mk2",
+	energy_required = 10,
+    enabled = false,
+	ingredients = 
+	{ 
+	  {"centrifuge", 1},
+	  {"steel-plate", 10},
+	  {"steel-gear-wheel", 25},
+	},
+   	result = "centrifuge-mk2",
+  },
+  {
+   	type = "recipe",
+   	name = "centrifuge-mk3",
+	energy_required = 10,
+    enabled = false,
+	ingredients = 
+	{ 
+	  {"centrifuge-mk2", 1},
+	  {"lead-plate", 10},
+	  {"tungsten-gear-wheel", 25},
+	},
+   	result = "centrifuge-mk3",
+  },
+  {
+   	type = "recipe",
+   	name = "centrifuge-mk4",
+	energy_required = 10,
+    enabled = false,
+	ingredients = 
+	{ 
+	  {"centrifuge-mk3", 1},
+	  {"tungsten-plate", 10},
+	  {"brass-gear-wheel", 25},
+	},
+   	result = "centrifuge-mk4",
+  },
+  {
+   	type = "recipe",
+   	name = "centrifuge-mk5",
+	energy_required = 10,
+    enabled = false,
+	ingredients = 
+	{ 
+	  {"centrifuge-mk4", 1},
+	  {"copper-tungsten-alloy", 10},
+	  {"copper-tungsten-gear-wheel", 25},
+	},
+   	result = "centrifuge-mk5",
   },
 }
 )
