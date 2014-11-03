044fe82e1b3d07535e346612fbd4879a4c10afda
Dysoch <frits@devreugd.nl>
2014-11-03 17:55:24 +0100

    [Warfare] Error fix

--- DyTech-Warfare/prototypes/base-edit/entity.lua
+++ DyTech-Warfare/prototypes/base-edit/entity.lua
@@ -26,7 +26,7 @@ data.raw["unit-spawner"]["spitter-spawner"].result_units = (function()
                      local res = {}
                      res[1] = {unit = "small-biter", spawn_points = {{evolution_factor=0, spawn_weight=0.3},
                                                                      {evolution_factor=0.25, spawn_weight=0}}} --stops at 0.25 evo
-                     res[2] = {"small-spitter", {{0.25, 0.3, {0.55, 0}}} --stops at 0.55 evo
+                     res[2] = {"small-spitter", {{0.25, 0.3}, {0.55, 0}}} --stops at 0.55 evo
                      res[3] = {"medium-spitter", {{0.45, 0.3}, {0.7, 0}}} --stops at 0.7 evo
                      res[4] = {"big-spitter", {{0.6, 0.0}, {1.0, 0.35}}} --35% max evo
 					 res[5] = {"berserk-biter", {{0.7, 0.0}, {1.0, 0.25}}} --25% max evo
