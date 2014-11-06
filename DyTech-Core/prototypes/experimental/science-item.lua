761480b2b47ddd43ed0f9d4b3ba4c76b8dca329e
Dysoch <frits@devreugd.nl>
2014-11-06 22:47:33 +0100

    [Core] 10 new science packs

--- DyTech-Core/prototypes/experimental/science-item.lua
+++ DyTech-Core/prototypes/experimental/science-item.lua
@@ -2,12 +2,93 @@ data:extend(
 {
   {
     type = "item",
-    name = "science-pack-4",
+    name = "science-pack-nature",
     icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
     flags = {"goes-to-main-inventory"},
     subgroup = "science-pack",
-    order = "a[science-pack-4]",
-    stack_size = 200
+    order = "b[science-pack-01]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-resource",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-02]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-industrial",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-03]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-fluid",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-04]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-tech",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-05]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-war",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-06]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-biter",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-07]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-advanced",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-08]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-nuclear",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-09]",
+    stack_size = 500
+  },
+  {
+    type = "item",
+    name = "science-pack-highly-advanced",
+    icon = "__DyTech-Core__/graphics/experimental/science-pack-4.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "science-pack",
+    order = "b[science-pack-10]",
+    stack_size = 500
   },
 }
 )
