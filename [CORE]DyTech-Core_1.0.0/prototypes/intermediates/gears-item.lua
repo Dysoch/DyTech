d75c3e22732c0839ae38fbd65b31b9b0e3274cdc
Dysoch <frits@devreugd.nl>
2015-01-09 10:22:57 +0100

    Gears

---
+++ [CORE]DyTech-Core_1.0.0/prototypes/intermediates/gears-item.lua
@@ -0,0 +1,22 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "stone-gear-wheel",
+    icon = "__[CORE]DyTech-Core__/graphics/intermediates/stone-gear-wheel.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "dytechgears",
+    order = "[a]stone",
+    stack_size = 100
+  },
+  {
+    type = "item",
+    name = "steel-gear-wheel",
+    icon = "__[CORE]DyTech-Core__/graphics/intermediates/steel-gear-wheel.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "dytechgears",
+    order = "[c]steel",
+    stack_size = 100
+  },
+}
+)
---
+++ [CORE]DyTech-Core_1.0.0/prototypes/intermediates/gears-recipe.lua
@@ -0,0 +1,23 @@
+data:extend(
+{ 
+  {
+    type = "recipe",
+    name = "stone-gear-wheel",
+    ingredients =
+    {
+      {"stone", 1},
+    },
+    result = "stone-gear-wheel"
+  },
+  {
+    type = "recipe",
+    name = "steel-gear-wheel",
+    enabled = false,
+    ingredients =
+    {
+      {"steel-plate", 1},
+    },
+    result = "steel-gear-wheel"
+  },
+}
+)
