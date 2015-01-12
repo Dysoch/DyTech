b1793649522dab8c078d6863e29ca1f3d969a7f3
Dysoch <frits@devreugd.nl>
2015-01-12 17:06:19 +0100

    Pumpjacks

--- [MAIN]DyTech-Machine_1.0.0/data.lua
+++ [MAIN]DyTech-Machine_1.0.0/data.lua
@@ -1,3 +1,19 @@
 -- Welcome to the data.lua file of DyTech-Machine!
 -- Here you can switch any module on or off!
 -- The general rule is: TRUE is on, FALSE is off!
+
+--[[ Pumpjacks ]]--
+-- Don't like the Pumpjacks? Here you can switch them off!
+Pumpjacks = true
+
+--[[ DO NOT TOUCH THE REST BELOW THIS LINE!!!! ]]--
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+--[[ Pumpjacks Check ]]--
+if Pumpjacks then 
+require("prototypes.pumpjacks.entity")
+require("prototypes.pumpjacks.item")
+require("prototypes.pumpjacks.recipe")
+require("prototypes.pumpjacks.tech")
+end
---
+++ [MAIN]DyTech-Machine_1.0.0/prototypes/pumpjacks/recipe.lua
@@ -0,0 +1,59 @@
+data:extend(
+{ 
+  {
+    type = "recipe",
+    name = "pumpjack-mk2",
+    energy_required = 20,
+    ingredients =
+    {
+      {"pumpjack", 1},
+      {"advanced-circuit", 10},
+      {"frame-1", 1},
+    },
+    result = "pumpjack-mk2",
+    enabled = false
+  },
+  {
+    type = "recipe",
+    name = "pumpjack-mk3",
+    energy_required = 20,
+    ingredients =
+    {
+      {"pumpjack-mk2", 1},
+      {"advanced-circuit", 20},
+      {"frame-2", 1},
+      {"lead-gear-wheel", 3},
+    },
+    result = "pumpjack-mk3",
+    enabled = false
+  },
+  {
+    type = "recipe",
+    name = "pumpjack-mk4",
+    energy_required = 20,
+    ingredients =
+    {
+      {"pumpjack-mk3", 1},
+      {"processing-unit", 20},
+      {"frame-3", 1},
+      {"steel-gear-wheel", 6},
+    },
+    result = "pumpjack-mk4",
+    enabled = false
+  },
+  {
+    type = "recipe",
+    name = "pumpjack-mk5",
+    energy_required = 20,
+    ingredients =
+    {
+      {"pumpjack-mk4", 1},
+      {"advanced-processing-unit", 20},
+      {"frame-4", 1},
+      {"tungsten-gear-wheel", 12},
+    },
+    result = "pumpjack-mk5",
+    enabled = false
+  },
+}
+)
