17425b4b14c3b472440c7001f704818427a6be95
Dysoch <frits@devreugd.nl>
2015-01-12 15:39:23 +0100

    Logistic Chest + Roboports

---
+++ [MAIN]DyTech-Machine_1.0.0/prototypes/logistic-network/roboport-recipe.lua
@@ -0,0 +1,30 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "roboport-1",
+    enabled = false,
+    ingredients =
+    {
+      {"lead-plate", 90},
+      {"lead-gear-wheel", 45},
+      {"processing-unit", 45}
+    },
+    result = "roboport-1",
+    energy_required = 30
+  },
+  {
+    type = "recipe",
+    name = "roboport-2",
+    enabled = false,
+    ingredients =
+    {
+      {"tungsten-plate", 180},
+      {"tungsten-gear-wheel", 90},
+      {"advanced-processing-unit", 45}
+    },
+    result = "roboport-2",
+    energy_required = 60
+  },
+}
+)
