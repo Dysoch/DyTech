afd8058f9aa6ea68421d7f2ca2ed069784d8d69c
Dysoch <frits@devreugd.nl>
2015-01-13 14:38:57 +0100

    Item Group + Locale

---
+++ [CORE]DyTech-Core_1.0.0/prototypes/item-group/inserter.lua
@@ -0,0 +1,59 @@
+data:extend(
+{
+  {
+    type = "item-group",
+    name = "inserters-dytech",
+    order = "aab",
+	inventory_order = "ca",
+    icon = "__base__/graphics/technology/inserter-stack-size-bonus.png"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-1",
+    group = "inserters-dytech",
+    order = "1"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-2",
+    group = "inserters-dytech",
+    order = "2"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-3",
+    group = "inserters-dytech",
+    order = "3"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-4",
+    group = "inserters-dytech",
+    order = "4"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-5",
+    group = "inserters-dytech",
+    order = "5"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-6",
+    group = "inserters-dytech",
+    order = "6"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-7",
+    group = "inserters-dytech",
+    order = "7"
+  },
+  {
+    type = "item-subgroup",
+    name = "inserters-dytech-8",
+    group = "inserters-dytech",
+    order = "8"
+  },
+}
+)
