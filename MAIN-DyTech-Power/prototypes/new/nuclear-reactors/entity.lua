55427afed256a981ea74fb560a721364a11c0339
MagicLegend <aapje112513@hotmail.nl>
2015-03-15 20:54:32 +0100

    Added nuclear-reactor files
    Nothing works yet :)

---
+++ MAIN-DyTech-Power/prototypes/new/nuclear-reactors/entity.lua
@@ -0,0 +1,26 @@
+data:extend({
+  {
+    type = "solar-panel",
+    name = "nuclear-reactor",
+    icon = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/icons/nuclear-reactor.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 2, mining_time = 4, result = "nuclear-reactor"},
+    max_health = 1000,
+    corpse = "big-remnants",
+    collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
+    selection_box = {{-3, -3}, {3, 3}},
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "primary-output"
+    },
+    picture =
+    {
+      filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+      priority = "high",
+      width = 104,
+      height = 96
+    },
+    production = "10MW"
+  }
+})
