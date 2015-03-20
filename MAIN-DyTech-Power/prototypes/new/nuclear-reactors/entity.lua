7a551bc4b866c6bb50d38d9408c1b8a7e390d3ee
MagicLegend <aapje112513@hotmail.nl>
2015-03-20 21:07:15 +0100

    Changed up the nuclear reactor code.
    Doesn't work properly yet.

--- MAIN-DyTech-Power/prototypes/new/nuclear-reactors/entity.lua
+++ MAIN-DyTech-Power/prototypes/new/nuclear-reactors/entity.lua
@@ -1,28 +1,60 @@
 data:extend(
 {
   {
-    type = "solar-panel",
+    type = "generator",
     name = "nuclear-reactor",
     icon = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/icons/nuclear-reactor.png",
     flags = {"placeable-neutral", "player-creation"},
-    minable = {hardness = 2, mining_time = 4, result = "nuclear-reactor"},
+    minable = {hardness = 0.2, mining_time = 4, result = "nuclear-reactor"},
     max_health = 1000,
     corpse = "big-remnants",
-    collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
+	effectivity = 1,
+	fluid_usage_per_tick = 0.08,
+    collision_box = {{-2.7, -2.7}, {2.7, 2.7}},
     selection_box = {{-3, -3}, {3, 3}},
+	fluid_box =
+	{
+		base_area = 1,
+		pipe_covers = pipecoverspictures(),
+		pipe_connections =
+		{
+			{ position = {0, 3.5} },
+			{ position = {0, -3.5} },
+			{ position = {3.5, 0} },
+			{ position = {-3.5, 0} },
+		},
+	},
     energy_source =
     {
       type = "electric",
       usage_priority = "primary-output"
     },
-    picture =
+    --[[picture =
     {
       filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
       priority = "high",
       width = 208,
       height = 192
-    },
-    production = "10MW"
+    },]]--
+	horizontal_animation =
+	{
+		filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+		width = 208,
+		height = 192,
+		frame_count = 1,
+		line_length = 1,
+		shift = {0, 0}
+	},
+	vertical_animation =
+	{
+		filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+		width = 208,
+		height = 192,
+		frame_count = 1,
+		line_length = 1,
+		shift = {0, 0}
+	},		
+    --production = "10MW"
   }
 }
 )
