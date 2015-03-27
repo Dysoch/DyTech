445ad47a58107e6113132f776932f4d78ac032b1
MagicLegend <aapje112513@hotmail.nl>
2015-03-27 20:14:02 +0100

    updated control.lua
    Working placement :smile:

--- MAIN-DyTech-Power/control.lua
+++ MAIN-DyTech-Power/control.lua
@@ -20,36 +20,55 @@ end
 
 --[[Insert Fancy Code Here:]]--
 
+glob.entitypos = {}
+glob.entityinfo = {}
+glob.entityinfo[1] = nuclear-reactor-container = {}
+nuclear-reactor-container[1] = false
 
 game.onevent(defines.events.onputitem, function(event)
-
 --Saving the coordinates of the placed entity
-	local x1 = event.onbuiltentity.position.x-2 --Left Top
-	local y1 = event.onbuiltentity.position.y+2
+	glob.entitypos[1] = event.position.x-2 --Left Top
+	glob.entitypos[2] = event.position.y+2
 	
-	local x2 = event.onbuiltentity.position.x+2 --Right Bottom
-	local y2 = event.onbuiltentity.position.y-2
+	glob.entitypos[4] = event.position.x+2 --Right Bottom
+	glob.entitypos[5] = event.position.y-2
 	
-	local cx1 = event.onbuiltentity.position.x-2
-	local cy1 = event.onbuiltentity.position.y
+	glob.entitypos[6] = event.position.x-3
+	glob.entitypos[7] = event.position.y
 end)
 
 --[[Reactor Code]]--
 
 game.onevent(defines.events.onbuiltentity, function(event)
 
-
 	
 	if event.createdentity.name == "nuclear-reactor" then
 		event.createdentity.operable = false
-		if (game.createentity{name = "nuclear-reactor-container", position = {cx1, cy1}, force=game.forces.player}) then
-			game.createentity{name = "nuclear-reactor-container", position = {cx1, cy1}, force=game.forces.player}
+		if (game.createentity{name = "nuclear-reactor-container", position = {glob.entitypos[6], glob.entitypos[7]}, force=game.forces.player}) then
+			game.createentity{name = "nuclear-reactor-container", position = {glob.entitypos[6], glob.entitypos[7]}, force=game.forces.player}
 		else
 			for i,player in pairs(game.players) do
 				player.print("The nuclear reactor couldn't be placed. Please make sure the complete 6x6 area is clear, and you place the reactor in the middle.")
 			end
-			event.createdentity.destroy()
+			glob.entitypos[10].destroy()
 			game.players[i].insert({name = "nuclear-reactor", count = 1})
 		end
 	end
+	
+	if event.createdentity.name == "nuclear-reactor-container" then
+		entityinfo[2] = game.createdentity()
+	end
 end)
+
+game.onevent(defines.events.onguiclick, function(event)
+
+	if event.guielement.name = "nuclear-reactor-container" then
+		nuclear-reactor-container[1] = true
+	end
+end)
+
+game.onevent(defines.events.ontick, function(event)
+
+	if nuclear-reactor-container[1] then
+		while i < 3600 do
+			
