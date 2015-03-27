6c7baa2c962985a02caaea11c0157df2c5fcd51c
MagicLegend <aapje112513@hotmail.nl>
2015-03-27 20:12:14 +0100

    Revert "Git strangeness... Updated some control.lua stuffz"
    This reverts commit fd46b4e6998257f46e7ad2b8e90c8a89e305eacc.

--- MAIN-DyTech-Power/prototypes/new/nuclear-reactors/entity.lua
+++ MAIN-DyTech-Power/prototypes/new/nuclear-reactors/entity.lua
@@ -1,155 +1,60 @@
 data:extend(
 {
   {
-	--Don't mind me:
-	--/c game.player.insert{name="nuclear-reactor",count=1}
-    type = "boiler",
+    type = "generator",
     name = "nuclear-reactor",
     icon = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/icons/nuclear-reactor.png",
     flags = {"placeable-neutral", "player-creation"},
     minable = {hardness = 0.2, mining_time = 4, result = "nuclear-reactor"},
     max_health = 1000,
     corpse = "big-remnants",
-	resistances =
-	{
-		{
-			type = "fire",
-			precent = 100
-		}
-	},
-    collision_box = {{-2.3, -2.3}, {2.3, 2.3}},
-    selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
+	effectivity = 1,
+	fluid_usage_per_tick = 0.08,
+    collision_box = {{-2.7, -2.7}, {2.7, 2.7}},
+    selection_box = {{-3, -3}, {3, 3}},
 	fluid_box =
 	{
-		base_area = 5,
+		base_area = 1,
 		pipe_covers = pipecoverspictures(),
 		pipe_connections =
 		{
-			{ position = {0, 3} },
-			{ position = {0, -3} },
-			{ position = {3, 0} },
-			{ position = {-3, 0} },
+			{ position = {0, 3.5} },
+			{ position = {0, -3.5} },
+			{ position = {3.5, 0} },
+			{ position = {-3.5, 0} },
 		},
 	},
-	energy_consumption = "1kW",
-	burner =
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "primary-output"
+    },
+    --[[picture =
+    {
+      filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+      priority = "high",
+      width = 208,
+      height = 192
+    },]]--
+	horizontal_animation =
 	{
-		effectivity = 1,
-		fuel_inventory_size = 1,
-		emissions = 0,
+		filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+		width = 208,
+		height = 192,
+		frame_count = 1,
+		line_length = 1,
+		shift = {0, 0}
 	},
-	fire =
+	vertical_animation =
 	{
-	  left = boilerfires.down,
-      down = boilerfires.left,
-      left_down = boilerfires.right,
-      right_down = boilerfires.left,
-      left_up = boilerfires.down,
-      right_up = boilerfires.down,
-      t_up = boilerfires.down
-	},
-	burning_cooldown = 20,
-	structure =
-	{
-		left =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		down =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		left_down =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		right_down =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		left_up =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		right_up =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		t_down =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		},
-		t_up =
-		{
-			filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
-			priority = "high",
-			width = 160,
-			height = 160,
-			--shift = {0.05, -1.18}
-		}
-    }
- },
-	{
-		type = "container",
-		name = "nuclear-reactor-container",
-		icon = "__base__/graphics/icons/wooden-chest.png",
-		flags = {"placeable-neutral", "player-creation"},
-		mineable = {mining_time = 1, result = ""},
-		max_health = 50,
-		corpse = "small-remnants",
-		collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-		fast_replaceable_group = "container",
-		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-		inventory_size = 1,
-		open_sound = { filename = "__base__/sound/wooden-chest-open.ogg" },
-		close_sound = { filename = "__base__/sound/wooden-chest-close.ogg" },
-		picture =
-		{
-			filename = "__base__/graphics/entity/wooden-chest/wooden-chest.png",
-			priority = "extra-high",
-			width = 46,
-			height = 33,
-			shift = {0.3, 0}
-		}
-	}
+		filename = "__MAIN-DyTech-Power__/graphics/nuclear-reactor/entity/nuclear-reactor.png",
+		width = 208,
+		height = 192,
+		frame_count = 1,
+		line_length = 1,
+		shift = {0, 0}
+	},		
+    --production = "10MW"
+  }
 }
 )
-
-
-
-
-
-
-
-
-
-
-
