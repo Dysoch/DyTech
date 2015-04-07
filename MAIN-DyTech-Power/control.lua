aecfe819d76f7977512245f16e4c6c7c03990ad6
MagicLegend <aapje112513@hotmail.nl>
2015-04-07 16:23:34 +0200

    cleanup with the help of dysoch

--- MAIN-DyTech-Power/control.lua
+++ MAIN-DyTech-Power/control.lua
@@ -34,26 +34,17 @@ function datadump(str, strname)
 end
 
 --[[Insert Fancy Code Here:]]--
-
-glob.entitypos = {}
-glob.entityinfo = {}
---[[glob.entityinfo[1] = nuclear-reactor-container = {}
-nuclear-reactor-container[1] = false]]--
+game.oninit(function()
 glob.tick = {}
 glob.tick[1] = 0
 glob.usedFuel = {}
+glob.entitypos = {}
+glob.entityinfo = {}
+glob.entitycount = 0
 
---[[game.onevent(defines.events.onputitem, function(event)
---Saving the coordinates of the placed entity
-	glob.entitypos[1] = event.position.x-2 --Left Top
-	glob.entitypos[2] = event.position.y+2
-	
-	glob.entitypos[4] = event.position.x+2 --Right Bottom
-	glob.entitypos[5] = event.position.y-2
-	
-	glob.entitypos[6] = event.position.x-3
-	glob.entitypos[7] = event.position.y
-end)]]--
+--glob.entitypos[glob.entitycount] = {}
+--glob.entityinfo[glob.entitycount] = {}
+end)
 
 --[[Reactor Code]]--
 
@@ -62,37 +53,37 @@ game.onevent(defines.events.onbuiltentity, function(event)
 	if event.createdentity.name == "nuclear-reactor" then
 	
 	--Saving the coordinates of the placed entity
-	glob.entitypos[1] = event.createdentity.position.x-2 --Left Top
-	glob.entitypos[2] = event.createdentity.position.y+2
+	glob.entitycount = glob.entitycount + 1
 	
-	glob.entitypos[4] = event.createdentity.position.x+2 --Right Bottom
-	glob.entitypos[5] = event.createdentity.position.y-2
+	glob.entitypos[glob.entitycount] = {}
+	glob.entitypos[glob.entitycount].TopLeft = event.createdentity.position.x-2 --Left Top
+	glob.entitypos[glob.entitycount].TopRight = event.createdentity.position.x+2
 	
-	glob.entitypos[6] = event.createdentity.position.x-3
-	glob.entitypos[7] = event.createdentity.position.y
+	glob.entitypos[glob.entitycount].BottomLeft = event.createdentity.position.y-2 --Right Bottom
+	glob.entitypos[glob.entitycount].BottomRight = event.createdentity.position.y+2
+	
+	glob.entitypos[glob.entitycount].ContainerX = event.createdentity.position.x-3
+	glob.entitypos[glob.entitycount].ContainerY = event.createdentity.position.y
 	
 	
-		glob.entityinfo[1] = event.createdentity
-		datadump(glob.entityinfo, "entityinfo1")
---[[		builtentityinfo = event.createdentity
-		table.insert(glob.entityinfo, builtentityinfo)
-		datadump(glob.entityinfo, "entityinfo1")]]--
+		glob.entityinfo[glob.entitycount] = {}
+		glob.entityinfo[glob.entitycount].ReactorEntity = event.createdentity
+		datadump(glob.entityinfo[glob.entitycount].ReactorEntity, "glob.entitycount"..glob.entitycount)
+		
 		event.createdentity.operable = false
-		if (game.canplaceentity{name = "nuclear-reactor-container", position = {glob.entitypos[6], glob.entitypos[7]}}) then
-			game.createentity{name = "nuclear-reactor-container", position = {glob.entitypos[6], glob.entitypos[7]}, force=game.forces.player}
+		if (game.canplaceentity{name = "nuclear-reactor-container", position = {glob.entitypos[glob.entitycount].ContainerX, glob.entitypos[glob.entitycount].ContainerY}}) then
+			game.createentity{name = "nuclear-reactor-container", position = {glob.entitypos[glob.entitycount].ContainerX, glob.entitypos[glob.entitycount].ContainerY}, force=game.forces.player}
 				debug("Entity Found!")
-				glob.entityinfo[2] = game.findentitiesfiltered{area = {{glob.entitypos[6], glob.entitypos[7]}, {glob.entitypos[6], glob.entitypos[7]}}, name = "nuclear-reactor-container"}
-				glob.entityinfo[4] = glob.entityinfo[2]
-				datadump(glob.entityinfo, "entityinfo")
-				datadump(glob.entitypos, "entitypos")
-				datadump(glob.entityinfo[2], "entityinfo[2]")
-				datadump(glob.entityinfo[4], "entityinfo[4]")
+				glob.entityinfo[glob.entitycount].ContainerEntity = game.findentitiesfiltered{area = {{glob.entitypos[glob.entitycount].ContainerX, glob.entitypos[glob.entitycount].ContainerY}, {glob.entitypos[glob.entitycount].ContainerX, glob.entitypos[glob.entitycount].ContainerY}}, name = "nuclear-reactor-container"}
+
+				datadump(glob.entityinfo[glob.entitycount], "Placed-Entity")
+
 		else
 			for i,player in ipairs(game.players) do
 				player.print("The nuclear reactor couldn't be placed. Please make sure the complete 6x6 area is clear, and you place the reactor in the middle.")
 				game.players[i].insert({name = "nuclear-reactor", count = 1})
 			end
-			glob.entityinfo[1].destroy()
+			glob.entityinfo[glob.entitycount].destroy()
 		end
 	end
 	
@@ -119,8 +110,9 @@ game.onevent(defines.events.ontick, function(event)
 end)
 
 function moveFuel()
-	if glob.entityinfo[2] ~= nil then
-		if glob.entityinfo[1].valid and glob.entityinfo[2].valid --[[and glob.entityinfo[2].name == "nuclear-reactor-container"]] then
+for i, entitycount in pairs(glob.entityinfo) do
+	if glob.entityinfo[i].ReactorEntity ~= nil and glob.entityinfo[i].ContainerEntity ~= nil then
+		if glob.entityinfo[i].ReactorEntity.valid and glob.entityinfo[i].ContainerEntity.valid --[[and glob.entityinfo[i].ContainerEntity.name == "nuclear-reactor-container"]] then
 			debug("It works!")
 		else
 			debug("Not valid")
@@ -132,20 +124,22 @@ function moveFuel()
 			debug("Ur a smartass u MagicLegend")
 				glob.entityinfo[5] = glob.entityinfo[2].getinventory(1).getcontents()
 			datadump(glob.entityinfo[5], "glob.entityinfo[5]")
-				glob.usedFuel[1] = glob.entityinfo[2].getinventory(1).getcontents()
+				glob.usedFuel[glob.entitycount] = glob.entityinfo[glob.entitycount].ContainerEntity.getinventory(1).getcontents()
 			debug("The usedFuel variable contains: "..usedFuel[1])
 				glob.entityinfo[2].removeitem(1)
 		end]]--
 	else
-	debug("glob.entityinfo[2] = nil")
+	debug("glob.entityinfo[i].ReactorEntity = nil or glob.entityinfo[i].ReactorEntity = nil")
 	end
+end
 end
 
 function calcEnergy()
-	if glob.usedFuel[1] ~= nil then
-		if glob.usedFuel[1] == "Plutonium" then
+for i, entitycount in pairs(glob.entityinfo) do
+	if glob.usedFuel[glob.entitycount] ~= nil then
+		if glob.usedFuel[glob.entitycount] == "Plutonium" then
 			debug("Plutonium all the way!")
-		elseif glob.usedFuel[1] == "Uranium" then
+		elseif glob.usedFuel[glob.entitycount] == "Uranium" then
 			debug("Uranium is your friend!")
 		else
 			debug("I have no idea what that is... But it doesn't look like something burnable in a reactor!")
@@ -154,6 +148,7 @@ function calcEnergy()
 	else
 		debug("No fuel")
 	end
+end
 end
 
 --Don't mind me:
