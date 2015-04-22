bae5cce87835afae46260d1595916ca6e5609de1
Dysoch <frits@devreugd.nl>
2015-04-22 08:56:59 +0200

    Cheats Updated to make them work

--- CORE-DyTech-Core/data-updates.lua
+++ CORE-DyTech-Core/data-updates.lua
@@ -2,40 +2,46 @@ require "config"
 
 if Infinite_Resources_Cheaty and not Infinite_Resources_Normal then
 	for k, v in pairs(data.raw.resource) do
-		data.raw.resource[k].infinite = true
-		data.raw.resource[k].minimum  = 17500
-		data.raw.resource[k].normal   = 35000
+		v.infinite = true
+		v.minimum = 17500
+		v.normal = 35000
+		v.autoplace.richness_base = 250000
 	end
 elseif Infinite_Resources_Normal and not Infinite_Resources_Cheaty then
 	for k, v in pairs(data.raw.resource) do
-		data.raw.resource[k].infinite = true
-		data.raw.resource[k].minimum  = 175
-		data.raw.resource[k].normal   = 350
+		v.infinite = true
+		v.minimum = 175
+		v.normal = 350
+		v.autoplace.richness_base = 2500
 	end
 end
 
 if Technology_Use_Less_Science_Packs then
 	for k, v in pairs(data.raw.technology) do
-		data.raw.technology[k].unit.count = 1
+    local TechCount = v.unit.count
+		TechCount = ((TechCount+1)-TechCount)
+		v.unit.count = TechCount
 	end
 end
 
 if Technology_Takes_Less_Time then
 	for k, v in pairs(data.raw.technology) do
-		data.raw.technology[k].unit.time = 1
+    local TechTime = v.unit.time
+		TechTime = ((TechTime+1)-TechTime)
+		v.unit.time = TechTime
 	end
 end
 
 if All_Recipes_Unlocked_From_Start then
 	for k, v in pairs(data.raw.recipe) do
-		if not data.raw.recipe[k].enabled then
-			data.raw.recipe[k].enabled = true
+		if not v.enabled then
+			v.enabled = true
 		end
 	end
 end
 
 if Stacksize_Increase then
 	for k, v in pairs(data.raw.item) do
-		data.raw.item[k].stack_size = (data.raw.item[k].stack_size*Stacksize_Increase_Factor)
+		v.stack_size = (v.stack_size*Stacksize_Increase_Factor)
 	end
 end
