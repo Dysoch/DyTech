ed594e0d47dfaa7f0c3b39ee1594e5c6aab25085
Dysoch <frits@devreugd.nl>
2015-04-23 13:51:20 +0200

    New Power Data.lua

--- MAIN-DyTech-Power/config.lua
+++ MAIN-DyTech-Power/config.lua
@@ -3,7 +3,7 @@
 -- The general rule is: TRUE is on, FALSE is off!
 
 -- Toggle between Old DyTech-Power(only steam engines, solars, accu's and boilers) and the new one with a lot more content.
-Use_Wattage = true
+Use_Wattage = false
 
 --[[Nuclear Reactors Toggle]]--
 -- Don't like the Nuclear Reactors? Here you can switch them off!
