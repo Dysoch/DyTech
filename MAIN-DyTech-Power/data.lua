21388a4795e4f482b4c351d8b653027b179d020e
Dysoch <frits@devreugd.nl>
2015-04-23 13:53:39 +0200

    conflicting data.lua fix

--- MAIN-DyTech-Power/data.lua
+++ MAIN-DyTech-Power/data.lua
@@ -1,6 +1,5 @@
 require "config"
 
-<<<<<<< HEAD
 if Nuclear_Reactors then
 	require("prototypes.new.nuclear-reactors.entity")
 	require("prototypes.new.nuclear-reactors.item")
@@ -19,11 +18,6 @@ end
 --[[ Boilers Check ]]--
 if Boilers then 
 	if Use_Wattage then
-=======
-if Use_Wattage then
-	--[[ Boilers Check ]]--
-	if Boilers then 
->>>>>>> origin/master
 		require("prototypes.new.boilers.entity")
 		require("prototypes.new.boilers.item")
 		require("prototypes.new.boilers.recipe")
@@ -64,7 +58,6 @@ if SolarPanels then
 		require("prototypes.old.solar-panels-normal.recipe")
 		require("prototypes.old.solar-panels-normal.tech")
 	end
-<<<<<<< HEAD
 end
 
 --[[ Steam Engines Check ]]--
@@ -75,53 +68,9 @@ if SteamEngines then
 		require("prototypes.new.steam-engines-normal.recipe")
 		require("prototypes.new.steam-engines-normal.tech")
 	else
-=======
-	--[[ Nuclear Rectors Check ]]--
-	if Nuclear_Reactors then
-		require("prototypes.new.nuclear-reactors.entity")
-		require("prototypes.new.nuclear-reactors.recipe")
-		require("prototypes.new.nuclear-reactors.tech")
-		require("prototypes.new.resources.uraniumdioxide")
---		require("prototypes.new.resources.fluorite")
-		require("prototypes.new.tile.noise-layers")
-		require("prototypes.new.nuclear-reactors.nuclear-entities.chemical-processor")
-		require("prototypes.new.nuclear-reactors.nuclear-items.fuel")
-		require("prototypes.new.nuclear-reactors.nuclear-items.items")
-		require("prototypes.new.nuclear-reactors.nuclear-items.reactor-item")
-		require("prototypes.new.nuclear-reactors.nuclear-tech.reactor-tech")
-	end
-else
--- hieronder is de oude DyTech-Power. Gaarne hier van af blijven :D
-	--[[ Boilers Check ]]--
-	if Boilers then 
-		require("prototypes.old.boilers.entity")
-		require("prototypes.old.boilers.item")
-		require("prototypes.old.boilers.recipe")
-		require("prototypes.old.boilers.tech")
-	end
-	--[[ Accumulators Check ]]--
-	if Accumulators then 
-		require("prototypes.old.accumulators-normal.entity")
-		require("prototypes.old.accumulators-normal.item")
-		require("prototypes.old.accumulators-normal.recipe")
-		require("prototypes.old.accumulators-normal.tech")
-	end
-	--[[ Steam Engines Check ]]--
-	if SteamEngines then 
->>>>>>> origin/master
 		require("prototypes.old.steam-engines-normal.entity")
 		require("prototypes.old.steam-engines-normal.item")
 		require("prototypes.old.steam-engines-normal.recipe")
 		require("prototypes.old.steam-engines-normal.tech")
 	end
-<<<<<<< HEAD
-=======
-	--[[ Solar Panels Check ]]--
-	if SolarPanels then 
-		require("prototypes.old.solar-panels-normal.entity")
-		require("prototypes.old.solar-panels-normal.item")
-		require("prototypes.old.solar-panels-normal.recipe")
-		require("prototypes.old.solar-panels-normal.tech")
-	end
->>>>>>> origin/master
 end
