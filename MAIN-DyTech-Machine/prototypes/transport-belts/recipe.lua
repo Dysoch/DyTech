0530fece56cda27400ef79cfa2cb8373747d9a2b
Dysoch <frits@devreugd.nl>
2015-05-31 12:06:57 +0200

    Changed recipe of Transport Belts to use lubricant

--- MAIN-DyTech-Machine/prototypes/transport-belts/recipe.lua
+++ MAIN-DyTech-Machine/prototypes/transport-belts/recipe.lua
@@ -3,24 +3,28 @@ data:extend(
   {
     type = "recipe",
     name = "super-transport-belt",
+    category = "crafting-with-fluid",
     enabled = false,
 	energy_required = 5,
     ingredients =
     {
 	  {"tungsten-gear-wheel", 2},
       {"express-transport-belt", 1}
+      {type="fluid", name="lubricant", amount=5},
     },
     result = "super-transport-belt"
   },
   {
     type = "recipe",
     name = "extreme-transport-belt",
+    category = "crafting-with-fluid",
     enabled = false,
 	energy_required = 6,
     ingredients =
     {
 	  {"cobalt-gear-wheel", 5},
       {"super-transport-belt", 1}
+      {type="fluid", name="lubricant", amount=10},
     },
     result = "extreme-transport-belt"
   },
