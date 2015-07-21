e4221e232d7e8c3c0a26c1c1f777511ff4d6c592
Dysoch <frits@devreugd.nl>
2015-07-21 10:46:59 +0200

    Radiation Capsule Graphics

--- MAIN-DyTech-War/prototypes/capsules/radiation/item.lua
+++ MAIN-DyTech-War/prototypes/capsules/radiation/item.lua
@@ -3,7 +3,7 @@ data:extend(
   {
     type = "capsule",
     name = "radiation-capsule-1",
-    icon = "__base__/graphics/icons/poison-capsule.png",
+    icon = "__MAIN-DyTech-War__/graphics/capsules/radiation/1.png",
     flags = {"goes-to-quickbar"},
     capsule_action =
     {
@@ -39,7 +39,7 @@ data:extend(
   {
     type = "capsule",
     name = "radiation-capsule-2",
-    icon = "__base__/graphics/icons/poison-capsule.png",
+    icon = "__MAIN-DyTech-War__/graphics/capsules/radiation/2.png",
     flags = {"goes-to-quickbar"},
     capsule_action =
     {
@@ -75,7 +75,7 @@ data:extend(
   {
     type = "capsule",
     name = "radiation-capsule-3",
-    icon = "__base__/graphics/icons/poison-capsule.png",
+    icon = "__MAIN-DyTech-War__/graphics/capsules/radiation/3.png",
     flags = {"goes-to-quickbar"},
     capsule_action =
     {
