d1e32a4058c867f9e59275f90fd3aaf1ef6e7a80
MagicLegend <aapje112513@hotmail.nl>
2015-07-27 17:09:23 +0200

    Fixed #326
    Also the game doesnt like my changes to the techs :)

--- MAIN-DyTech-Power/prototypes/boilers/tech/highTech.lua
+++ MAIN-DyTech-Power/prototypes/boilers/tech/highTech.lua
@@ -1,35 +1,44 @@
-Dytech_Create_Tech_Power({
+DyTech_Create_Tech_Power({
 Name = "boilers-1",
 Icon = "/boilers/boiler2.png",
 Count = 150,
-Time = 30,
+Time = 10,
 Prereq = {"tin-processing", "rotor-1"},
-Recipes = {"high-boiler-mk2"},
+Recipes = {"high-boiler-mk1"},
 Ingredients = {Science1 = true, Science2 = false, Science3 = false, Science4 = false}})
 
-Dytech_Create_Tech_Power({
+DyTech_Create_Tech_Power({
 Name = "boilers-2",
-Icon = "/boilers/boiler2.png",
-Count = 225,
-Time = 60,
-Prereq = {"boilers-1", "lead-processing", "rotor-2"},
-Recipes = {"high-boiler-mk3"},
-Ingredients = {Science1 = true, Science2 = true, Science3 = false, Science4 = false}})
-
-Dytech_Create_Tech_Power({
-Name = "boilers-3",
-Icon = "/boilers/boiler2.png",
-Count = 300,
-Time = 60,
-Prereq = {"boilers-2", "steel-processing"},
-Recipes = {"high-boiler-mk4"},
-Ingredients = {Science1 = true, Science2 = true, Science3 = true, Science4 = false}})
-
-Dytech_Create_Tech_Power({
-Name = "boilers-4",
 Icon = "/boilers/boiler2.png",
 Count = 150,
 Time = 30,
-Prereq = {"boilers-1", "lead-processing", "rotor-2"},
+Prereq = {"boilers-1"},
+Recipes = {"high-boiler-mk2"},
+Ingredients = {Science1 = true, Science2 = false, Science3 = false, Science4 = false}})
+
+DyTech_Create_Tech_Power({
+Name = "boilers-3",
+Icon = "/boilers/boiler2.png",
+Count = 225,
+Time = 60,
+Prereq = {"boilers-2", "lead-processing", "rotor-2"},
+Recipes = {"high-boiler-mk3"},
+Ingredients = {Science1 = true, Science2 = true, Science3 = false, Science4 = false}})
+
+DyTech_Create_Tech_Power({
+Name = "boilers-4",
+Icon = "/boilers/boiler2.png",
+Count = 300,
+Time = 60,
+Prereq = {"boilers-3", "steel-processing"},
+Recipes = {"high-boiler-mk4"},
+Ingredients = {Science1 = true, Science2 = true, Science3 = true, Science4 = false}})
+
+DyTech_Create_Tech_Power({
+Name = "boilers-5",
+Icon = "/boilers/boiler2.png",
+Count = 150,
+Time = 30,
+Prereq = {"boilers-4"},
 Recipes = {"high-boiler-mk5"},
 Ingredients = {Science1 = true, Science2 = true, Science3 = true, Science4 = false}})
