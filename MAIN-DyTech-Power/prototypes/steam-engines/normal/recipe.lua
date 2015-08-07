54951a7780140aeb658e0f4ca3faefadea133a74
MagicLegend <aapje112513@hotmail.nl>
2015-08-07 21:28:27 +0200

    Fcked up somewhere
    no idea where tho...

--- MAIN-DyTech-Power/prototypes/steam-engines/normal/recipe.lua
+++ MAIN-DyTech-Power/prototypes/steam-engines/normal/recipe.lua
@@ -1,41 +1,5 @@
 data:extend(
 {
---[[  {
-    type = "recipe",
-    name = "steam-engine-primary",
-    ingredients =
-    {
-      {"stone-gear-wheel", 5},
-      {"pipe", 5},
-      {"iron-plate", 5}
-    },
-    result = "steam-engine-primary"
-  },
-  {
-    type = "recipe",
-    name = "steam-engine-terciary",
-    ingredients =
-    {
-      {"stone-gear-wheel", 5},
-      {"pipe", 5},
-      {"iron-plate", 5}
-    },
-    result = "steam-engine-terciary"
-  },
-  {
-    type = "recipe",
-    name = "steam-engine-primary-mk2",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-primary", 1},
-      {"flux-capacitor-1", 2},
-      {"frame-1", 1},
-      {"rotor-1", 4},
-      {"electronic-circuit", 5}
-    },
-    result = "steam-engine-primary-mk2"
-  },]]
   {
     type = "recipe",
     name = "steam-engine-secondary-mk2",
@@ -50,34 +14,6 @@ data:extend(
     },
     result = "steam-engine-secondary-mk2"
   },
---[[  {
-    type = "recipe",
-    name = "steam-engine-terciary-mk2",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-terciary", 1},
-      {"flux-capacitor-1", 2},
-      {"frame-1", 1},
-      {"rotor-1", 4},
-      {"electronic-circuit", 5}
-    },
-    result = "steam-engine-terciary-mk2"
-  },
-  {
-    type = "recipe",
-    name = "steam-engine-primary-mk3",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-primary-mk2", 1},
-      {"flux-capacitor-2", 2},
-      {"frame-2", 1},
-      {"rotor-2", 4},
-      {"advanced-circuit", 5}
-    },
-    result = "steam-engine-primary-mk3"
-  },]]
   {
     type = "recipe",
     name = "steam-engine-secondary-mk3",
@@ -92,34 +28,6 @@ data:extend(
     },
     result = "steam-engine-secondary-mk3"
   },
---[[  {
-    type = "recipe",
-    name = "steam-engine-terciary-mk3",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-terciary-mk2", 1},
-      {"flux-capacitor-2", 2},
-      {"frame-2", 1},
-      {"rotor-2", 4},
-      {"advanced-circuit", 5}
-    },
-    result = "steam-engine-terciary-mk3"
-  },
-  {
-    type = "recipe",
-    name = "steam-engine-primary-mk4",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-primary-mk3", 1},
-      {"flux-capacitor-3", 5},
-      {"frame-3", 1},
-      {"rotor-3", 4},
-      {"processing-unit", 5}
-    },
-    result = "steam-engine-primary-mk4"
-  },]]
   {
     type = "recipe",
     name = "steam-engine-secondary-mk4",
@@ -134,34 +42,6 @@ data:extend(
     },
     result = "steam-engine-secondary-mk4"
   },
---[[  {
-    type = "recipe",
-    name = "steam-engine-terciary-mk4",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-terciary-mk3", 1},
-      {"flux-capacitor-3", 5},
-      {"frame-3", 1},
-      {"rotor-3", 4},
-      {"processing-unit", 5}
-    },
-    result = "steam-engine-terciary-mk4"
-  },
-  {
-    type = "recipe",
-    name = "steam-engine-primary-mk5",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-primary-mk4", 1},
-      {"flux-capacitor-4", 10},
-      {"frame-4", 1},
-      {"rotor-4", 4},
-      {"advanced-processing-unit", 5}
-    },
-    result = "steam-engine-primary-mk5"
-  },]]
   {
     type = "recipe",
     name = "steam-engine-secondary-mk5",
@@ -176,19 +56,5 @@ data:extend(
     },
     result = "steam-engine-secondary-mk5"
   },
---[[  {
-    type = "recipe",
-    name = "steam-engine-terciary-mk5",
-	enabled = false,
-    ingredients =
-    {
-      {"steam-engine-terciary-mk4", 1},
-      {"flux-capacitor-4", 10},
-      {"frame-4", 1},
-      {"rotor-4", 4},
-      {"advanced-processing-unit", 5}
-    },
-    result = "steam-engine-terciary-mk5"
-  },]]
 }
 )
