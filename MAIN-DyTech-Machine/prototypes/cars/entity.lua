c4850fc4e2f4118e566d6ca35bff816ff316889d
Dysoch <frits@devreugd.nl>
2015-08-09 09:07:43 +0200

    Fixes #372

--- MAIN-DyTech-Machine/prototypes/cars/entity.lua
+++ MAIN-DyTech-Machine/prototypes/cars/entity.lua
@@ -1,25 +1,28 @@
 require "prototypes.internal-config"
+CAR2 = {r=0.698, g=0.133, b=0.133, a=0.95}
 
 data:extend(
 {
   {
     type = "car",
     name = "car2",
-    icon = "__MAIN-DyTech-Machine__/graphics/cars/car2.png",
+    icon = "__base__/graphics/icons/car.png",
     flags = {"pushable", "placeable-neutral", "player-creation", "placeable-off-grid"},
     minable = {mining_time = 1, result = "car2"},
-    max_health = Health.Tier5,
+    max_health = Health.Tier7,
     corpse = "medium-remnants",
     dying_explosion = "medium-explosion",
-    resistances = Resistances.Tier4,
+    energy_per_hit_point = 2,
+    crash_trigger = crash_trigger(),
+    resistances = Resistances.Tier7,
     collision_box = {{-0.7, -1}, {0.7, 1}},
     selection_box = {{-0.7, -1}, {0.7, 1}},
-    effectivity = 0.1,
-    braking_power = "300kW",
+    effectivity = 0.5,
+    braking_power = "500kW",
     burner =
     {
-      effectivity = 0.8,
-      fuel_inventory_size = 2,
+      effectivity = 0.5,
+      fuel_inventory_size = 1,
       smoke =
       {
         {
@@ -27,7 +30,6 @@ data:extend(
           deviation = {0.25, 0.25},
           frequency = 50,
           position = {0, 1.5},
-          slow_down_factor = 0.9,
           starting_frame = 3,
           starting_frame_deviation = 5,
           starting_frame_speed = 0,
@@ -35,9 +37,8 @@ data:extend(
         }
       }
     },
-    consumption = "600kW",
-    friction = 0.04,
-    energy_per_hit_point = 3,
+    consumption = "250kW",
+    friction = 2e-3,
     light =
     {
       {
@@ -73,14 +74,130 @@ data:extend(
     },
     animation =
     {
-      filename = "__MAIN-DyTech-Machine__/graphics/cars/car-sheet.png",
-      line_length = 8,
-      width = 130,
-      height = 93,
-      frame_count = 1,
-      shift={0.5, 0},
-      axially_symmetrical = false,
-      direction_count = 64
+      layers =
+      {
+        {
+          width = 102,
+          height = 86,
+          frame_count = 2,
+          direction_count = 64,
+          shift = {0, -0.1875},
+          animation_speed = 8,
+          max_advance = 0.2,
+          stripes =
+          {
+            {
+             filename = "__base__/graphics/entity/car/car-1.png",
+             width_in_frames = 2,
+             height_in_frames = 22,
+			 tint = CAR2
+            },
+            {
+             filename = "__base__/graphics/entity/car/car-2.png",
+             width_in_frames = 2,
+             height_in_frames = 22,
+			 tint = CAR2
+            },
+            {
+             filename = "__base__/graphics/entity/car/car-3.png",
+             width_in_frames = 2,
+             height_in_frames = 20,
+			 tint = CAR2
+            },
+          }
+        },
+        {
+          width = 100,
+          height = 75,
+          frame_count = 2,
+          apply_runtime_tint = true,
+          direction_count = 64,
+          max_advance = 0.2,
+          line_length = 2,
+          shift = {0, -0.171875},
+          stripes = util.multiplystripes(2,
+          {
+            {
+              filename = "__base__/graphics/entity/car/car-mask-1.png",
+              width_in_frames = 1,
+              height_in_frames = 22,
+            },
+            {
+              filename = "__base__/graphics/entity/car/car-mask-2.png",
+              width_in_frames = 1,
+              height_in_frames = 22,
+            },
+            {
+              filename = "__base__/graphics/entity/car/car-mask-3.png",
+              width_in_frames = 1,
+              height_in_frames = 20,
+            },
+          })
+        },
+        {
+          width = 114,
+          height = 76,
+          frame_count = 2,
+          draw_as_shadow = true,
+          direction_count = 64,
+          shift = {0.28125, 0.25},
+          max_advance = 0.2,
+          stripes = util.multiplystripes(2,
+          {
+           {
+            filename = "__base__/graphics/entity/car/car-shadow-1.png",
+            width_in_frames = 1,
+            height_in_frames = 22,
+           },
+           {
+            filename = "__base__/graphics/entity/car/car-shadow-2.png",
+            width_in_frames = 1,
+            height_in_frames = 22,
+           },
+           {
+            filename = "__base__/graphics/entity/car/car-shadow-3.png",
+            width_in_frames = 1,
+            height_in_frames = 20,
+           },
+          })
+        }
+      }
+    },
+    turret_animation =
+    {
+      layers =
+      {
+        {
+          filename = "__base__/graphics/entity/car/car-turret.png",
+          line_length = 8,
+          width = 36,
+          height = 29,
+          frame_count = 1,
+          direction_count = 64,
+          shift = {0.03125, -0.890625},
+          animation_speed = 8,
+		  tint = CAR2
+        },
+        {
+          filename = "__base__/graphics/entity/car/car-turret-shadow.png",
+          line_length = 8,
+          width = 46,
+          height = 31,
+          frame_count = 1,
+          draw_as_shadow = true,
+          direction_count = 64,
+          shift = {0.875, 0.359375},
+		  tint = CAR2
+        }
+      }
+    },
+    turret_rotation_speed = 0.35 / 60,
+    sound_no_fuel =
+    {
+      {
+        filename = "__base__/sound/fight/car-no-fuel-1.ogg",
+        volume = 0.6
+      },
     },
     stop_trigger_speed = 0.2,
     stop_trigger =
@@ -96,8 +213,8 @@ data:extend(
         }
       },
     },
-    crash_trigger = crash_trigger(),
     sound_minimum_speed = 0.2;
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
     working_sound =
     {
       sound =
@@ -119,9 +236,10 @@ data:extend(
     },
     open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
     close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
-    rotation_speed = 0.010,
-    weight = 1500,
-    inventory_size = 50
+    rotation_speed = 0.015,
+    weight = 950,
+    guns = { "vehicle-machine-gun" },
+    inventory_size = 80
   },
 }
 )
